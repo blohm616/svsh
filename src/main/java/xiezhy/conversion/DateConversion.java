f02e8c03709502ea58f7f5193f6328dc33abe3b8
xiezhy <bin_cruel@sohu.com>
2016-04-25 11:17:09 +0800

    添加

---
+++ src/main/java/xiezhy/conversion/DateConversion.java
@@ -0,0 +1,18 @@
+package xiezhy.conversion;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.Date;
+
+/**
+ * version:
+ * --------------------
+ * date:2016/4/25
+ * author:binbin
+ */
+public class DateConversion implements Converter<String,Date> {
+    @Override
+    public Date convert(String s) {
+        return null;
+    }
+}
